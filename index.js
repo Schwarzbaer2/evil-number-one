7c06d3bbe8714edfbb1d23074d444e72e12901ce
Andreas Schwarz <andreas.schwarz20@posteo.de>
2020-08-17 17:07:07 +0200

    fixing the message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
-let turn = 0
+let turn = 1
 
 // Create variables to store references to the necessary DOM nodes
 const player1Dice = document.getElementById("player1Dice")
@@ -21,33 +21,33 @@ function showResetButton() {
 function gameLogic() {
     turn++;
     const randomNumber = Math.floor(Math.random() * 6) + 1
+
     if (player1Turn) {
-        message.textContent = `Player 1 Turn ${turn + 1}`
+
         if (randomNumber == 1) {
             message.textContent = "😈Evil Number One😈"
-            player1Score -= 10;
+            player1Score -=10;
             player1Scoreboard.textContent = player1Score
             player1Dice.textContent = randomNumber
         } else {
+        message.textContent = `Player 1 Turn ${turn}`
        player1Score += randomNumber
        player1Scoreboard.textContent = player1Score
        player1Dice.textContent = randomNumber
         }
-
    } else {
-       message.textContent = `Player 2 Turn ${turn + 1}`
        if (randomNumber == 1) {
         message.textContent = "😈Evil Number One😈"
-        player2Score -= 10;
-        player2Scoreboard.textContent = player1Score
+        player2Score -=10;
+        player2Scoreboard.textContent = player2Score
         player2Dice.textContent = randomNumber
     } else {
+        message.textContent = `Player 2 Turn ${turn}`
        player2Score += randomNumber
        player2Scoreboard.textContent = player2Score
        player2Dice.textContent = randomNumber
     }
    }
-
    if (player1Score >= 20) {
        message.textContent = "Player 1 Won 🥳"
        showResetButton()
@@ -55,18 +55,19 @@ function gameLogic() {
        message.textContent = "Player 2 Won 🎉"
        showResetButton()
    }
-   if (turn == 5 && player1Turn) {
-       turn = 0
+
+   if (player1Turn && turn == 5) {
        player1Dice.classList.remove("active")
        player2Dice.classList.add("active")
        message.textContent = `Player 2 Turn 1`
        player1Turn = !player1Turn
-   } else if (turn == 5 && !player1Turn) {
        turn = 0
-    player2Dice.classList.remove("active")
-    player1Dice.classList.add("active")
-    message.textContent = `Player 1 Turn 1`
-    player1Turn = !player1Turn
+   } else if (!player1Turn && turn == 5) {
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = `Player 1 Turn 1`
+        player1Turn = !player1Turn
+        turn = 0
    }
 }
 
@@ -80,7 +81,6 @@ reset();
 /* Hook up a click event listener to the Roll Dice Button. */
 rollBtn.addEventListener("click", function() {
    gameLogic();
-
 })
 
 resetBtn.addEventListener("click", function(){
@@ -91,12 +91,12 @@ function reset() {
     player1Score = 0
     player2Score = 0
     player1Turn = true
-    turn = 0
+    turn = 1
     player1Scoreboard.textContent = 0
     player2Scoreboard.textContent = 0
     player1Dice.textContent = "-"
     player2Dice.textContent = "-"
-    message.textContent = "Player 1 Turn 1"
+    message.textContent = `Player 1 Turn ${turn}`
     playersHidden.style.display ="flex"
     introduction.style.display = "none"
     startBtn.style.display = "none"
